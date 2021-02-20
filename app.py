30f79c4d44ab2530368e2fb0eabf3000ac4842eb
Vasu Bajaj <41167055+VasuBajaj@users.noreply.github.com>
2021-02-20 09:09:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import pickle
 import logging
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score,mean_squared_error
